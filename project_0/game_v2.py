e83d776797dd4deb0795d25632c0bb7ada56d959
igorpuzik <igor.puzik@mail.ru>
2022-11-09 16:52:33 +0300

    editing game_v2.py by pip 8

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -5,7 +5,7 @@
 import numpy as np
 
 
-def random_predict(number: int = 1) -> int:
+def random_predict(number: int=1) -> int:
     """Рандомно угадываем число
 
     Args:
@@ -30,7 +30,7 @@ def random_predict(number: int = 1) -> int:
           min = mid
 
         else:
-            break #конец игры выход из цикла
+            break #Конец игры выход из цикла
     return count
 
 
@@ -44,8 +44,8 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    np.random.seed(1)  # Фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # Загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
