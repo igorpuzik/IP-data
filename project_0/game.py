4bed0a280f21222cb1d7233e53935e7c91d6dcbd
igorpuzik <igor.puzik@mail.ru>
2022-11-10 15:12:49 +0300

    replacing game_v2.py by game.py

--- project_0/game.py
+++ project_0/game.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 def random_predict(number: int=1) -> int:
-    """Рандомно угадываем число
+    """Медианно угадываем число
 
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
